e717ca15972a24df69d03d8f7fcf9c1d04a72aad
Ruben Ortiz <ruben.ortiz@fcc-fac.ca>
2023-02-13 14:54:24 -0600

    Queen.moveTo refactoring

--- src/main/java/ca/attractors/chess/Queen.java
+++ src/main/java/ca/attractors/chess/Queen.java
@@ -1,7 +1,93 @@
 package ca.attractors.chess;
 
+import java.util.ArrayList;
+import java.util.List;
+
 public class Queen extends ChessPiece {
+
     public Queen(PieceColor color, Board board, Position position) {
         super(color, board, position);
     }
+
+    /**
+     *
+     * @param targetPosition - the position that we would like to move to
+     * @return true if we were able to complete the move. false otherwise
+     */
+    public boolean moveTo(Position targetPosition) {
+        if (isValidMovement(targetPosition)) {
+            getChessboard().movePieceTo(this, targetPosition);
+            return true;
+        }
+        return false;
+    }
+
+    private boolean isValidMovement(Position position) {
+        if (!isTargetSquareAvailable(position)) {
+            return false;
+        }
+        
+        if (position.y == getPosition().y && !isFreeToMoveVertically(position)) {
+            return false;
+        }
+        
+        if (position.x == getPosition().x && !isFreeToMoveHorizontally(position)) {
+            return false;
+        }
+
+        if (position.x != getPosition().x && position.y != getPosition().y && !isFreeToMoveDiagonally(position)) {
+            return false;
+        }
+
+        return true;
+    }
+
+    private boolean isTargetSquareAvailable(Position targetPosition) {
+        ChessPiece targetPiece = getChessboard().getPieceAt(targetPosition);
+        return targetPiece == null || targetPiece.getColor() != getColor();
+    }
+
+    private boolean isFreeToMoveHorizontally(Position targetPosition) {
+        int start = getPosition().getYOffset();
+        int end = targetPosition.getYOffset();
+        int increment = start > end ? -1 : 1;
+        List<Position> positions = new ArrayList<>();
+        for (int y = start + increment; y != end; y = y + increment) {
+            positions.add(Position.getPositionFor(targetPosition.getXOffset(), y));
+        }
+        for (Position position: positions) {
+            if (getChessboard().getPieceAt(position) != null) {
+                return false;
+            }
+        }
+        return true;
+    }
+
+    private boolean isFreeToMoveVertically(Position targetPosition) {
+        //Next - Get all the cells between the source and the target and ensure that they are empty.
+        // if this is a vertical move we need to increment the x coordinate until it is the same as the target's x
+        // the increment might be positive or negative.
+        int start = getPosition().getXOffset();
+        int end = targetPosition.getXOffset();
+        int increment = start > end ? -1 : 1;
+        List<Position> positions = new ArrayList<>();
+        for (int x = start+increment; x != end; x = x + increment) {
+            positions.add(Position.getPositionFor(x, targetPosition.getYOffset()));
+        }
+        for (Position position: positions) {
+            if (getChessboard().getPieceAt(position) != null) {
+                return false;
+            }
+        }
+        return true;
+    }
+
+    private boolean isFreeToMoveDiagonally(Position targetPosition) {
+        int x = getPosition().x;
+        int y = getPosition().y;
+        int targetX = targetPosition.x;
+        int targetY = targetPosition.y;
+        return Math.abs(x - targetX) == Math.abs(y - targetY);
+    }
+
 }
--- src/main/java/ca/attractors/chess/Rook.java
+++ src/main/java/ca/attractors/chess/Rook.java
@@ -14,19 +14,26 @@ public class Rook extends ChessPiece {
      * @return true if we were able to complete the move. false otherwise
      */
     public boolean moveTo(Position targetPosition) {
-        if (!isRankEquals(targetPosition) && !isFileEquals(targetPosition)) {
+        if (isValidMovement(targetPosition)) {
+            getChessboard().movePieceTo(this, targetPosition);
+            return true;
+        }
+        return false;
+    }
+
+    private boolean isValidMovement(Position position) {
+        if (!isRankEquals(position) && !isFileEquals(position)) {
             return false;
         }
 
-        if (!isTargetSquareAvailable(targetPosition)) {
+        if (!isTargetSquareAvailable(position)) {
             return false;
         }
         
-        if (!isFreeToMoveHorizontally(targetPosition) || !isFreeToMoveVertically(targetPosition)) {
+        if (!isFreeToMoveHorizontally(position) || !isFreeToMoveVertically(position)) {
             return false;
         }
 
-        getChessboard().movePieceTo(this, targetPosition);
         return true;
     }
 
@@ -44,25 +51,19 @@ public class Rook extends ChessPiece {
     }
 
     private boolean isFreeToMoveHorizontally(Position targetPosition) {
-        //Next - Get all the cells between the source and the target and ensure that they are empty.
-        // if this is a horizontal move we need to increment the y coordinate until it is the same as the target's y
-        // the increment might be positive or negative.
-        if (targetPosition.x == getPosition().x) {
-            int start = getPosition().getYOffset();
-            int end = targetPosition.getYOffset();
-            int increment = 0;
-            if (start > end)
-                increment = -1;
-            else
-                increment = 1;
-            List<Position> positions = new ArrayList<>();
-            for (int y = start + increment; y != end; y = y + increment) {
-                positions.add(Position.getPositionFor(targetPosition.getXOffset(), y));
-            }
-            for (Position position: positions) {
-                if (getChessboard().getPieceAt(position) != null) {
-                    return false;
-                }
+        if (targetPosition.x != getPosition().x) {
+            return true;
+        }
+        int start = getPosition().getYOffset();
+        int end = targetPosition.getYOffset();
+        int increment = start > end ? -1 : 1;
+        List<Position> positions = new ArrayList<>();
+        for (int y = start + increment; y != end; y = y + increment) {
+            positions.add(Position.getPositionFor(targetPosition.getXOffset(), y));
+        }
+        for (Position position: positions) {
+            if (getChessboard().getPieceAt(position) != null) {
+                return false;
             }
         }
         return true;
@@ -72,22 +73,19 @@ public class Rook extends ChessPiece {
         //Next - Get all the cells between the source and the target and ensure that they are empty.
         // if this is a vertical move we need to increment the x coordinate until it is the same as the target's x
         // the increment might be positive or negative.
-        if (targetPosition.y == getPosition().y) {
-            int start = getPosition().getXOffset();
-            int end = targetPosition.getXOffset();
-            int increment = 0;
-            if (start > end)
-                increment = -1;
-            else
-                increment = 1;
-            List<Position> positions = new ArrayList<>();
-            for (int x = start+increment; x != end; x = x + increment) {
-                positions.add(Position.getPositionFor(x, targetPosition.getYOffset()));
-            }
-            for (Position position: positions) {
-                if (getChessboard().getPieceAt(position) != null) {
-                    return false;
-                }
+        if (targetPosition.y != getPosition().y) {
+            return true;
+        }
+        int start = getPosition().getXOffset();
+        int end = targetPosition.getXOffset();
+        int increment = start > end ? -1 : 1;
+        List<Position> positions = new ArrayList<>();
+        for (int x = start+increment; x != end; x = x + increment) {
+            positions.add(Position.getPositionFor(x, targetPosition.getYOffset()));
+        }
+        for (Position position: positions) {
+            if (getChessboard().getPieceAt(position) != null) {
+                return false;
             }
         }
         return true;
