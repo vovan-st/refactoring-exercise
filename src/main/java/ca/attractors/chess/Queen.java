0396ba68413efd9f179684e9bd6e85a9f56a85ff
dpasion <pasiond@fcc.ca>
2023-02-13 14:35:06 -0600

    feat: start validation

--- src/main/java/ca/attractors/chess/Queen.java
+++ src/main/java/ca/attractors/chess/Queen.java
@@ -4,4 +4,20 @@ public class Queen extends ChessPiece {
     public Queen(PieceColor color, Board board, Position position) {
         super(color, board, position);
     }
+
+    public boolean moveTo(Position targetPosition) {
+        ChessPiece targetPiece = getChessboard().getPieceAt(targetPosition);
+        if (!isTargetPositionValidMove(targetPosition)) return false;
+//        if (isTargetPieceSameColour(targetPiece)) return false;
+//        if (!isTargetPathValid(targetPosition)) return false;
+        getChessboard().movePieceTo(this, targetPosition);
+        return true;
+    }
+
+    private boolean isTargetPositionValidMove(Position targetPosition){
+        boolean verticalMoveValid = true;
+        boolean horizontalMoveValid = true;
+        boolean diagonalMoveValid = true;
+        return verticalMoveValid && horizontalMoveValid && diagonalMoveValid;
+    }
 }
