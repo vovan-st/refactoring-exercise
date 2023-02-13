85ef19b018079c60eb7030e4bf59c6192531d063
Mike Mogentale <mlmogentale@gmail.com>
2023-02-13 13:04:25 -0600

    refactor start

--- src/main/java/ca/attractors/chess/Rook.java
+++ src/main/java/ca/attractors/chess/Rook.java
@@ -15,26 +15,20 @@ public class Rook extends ChessPiece {
      */
     public boolean moveTo(Position targetPosition) {
         //if it is not the same x or y coordinate it is not a rooks valid move at all
-        if (targetPosition.x != getPosition().x && targetPosition.y != getPosition().y) {
+        if (!validRookMove(targetPosition)) {
             return false;
         }
         //Next - Check to make sure that if the target square is occupied it is not the same color
-        ChessPiece targetPiece = getChessboard().getPieceAt(targetPosition);
-        if (targetPiece != null) {
-            if (targetPiece.getColor() == getColor())
-                return false;
-        }
+        if (getPositionOccupiedWithSameColor(targetPosition))
+            return false;
+
         //Next - Get all the cells between the source and the target and ensure that they are empty.
         // if this is a horizontal move we need to increment the y coordinate until it is the same as the target's y
         // the increment might be positive or negative.
         if (targetPosition.x == getPosition().x) {
             int start = getPosition().getYOffset();
             int end = targetPosition.getYOffset();
-            int increment = 0;
-            if (start > end)
-                increment = -1;
-            else
-                increment = 1;
+            int increment = getIncrement(targetPosition)
             List<Position> positions = new ArrayList<>();
             for (int y = start+increment; y != end; y = y + increment) {
                 positions.add(Position.getPositionFor(targetPosition.getXOffset(), y));
@@ -51,11 +45,7 @@ public class Rook extends ChessPiece {
         if (targetPosition.y == getPosition().y) {
             int start = getPosition().getXOffset();
             int end = targetPosition.getXOffset();
-            int increment = 0;
-            if (start > end)
-                increment = -1;
-            else
-                increment = 1;
+            int increment = getIncrement(targetPosition);
             List<Position> positions = new ArrayList<>();
             for (int x = start+increment; x != end; x = x + increment) {
                 positions.add(Position.getPositionFor(x, targetPosition.getYOffset()));
@@ -66,9 +56,30 @@ public class Rook extends ChessPiece {
                 }
             }
         }
-        //If we get here - is is a valid move. Physically move the piece and answer true.
         getChessboard().movePieceTo(this, targetPosition);
         return true;
+    }
+
+    private int getIncrement(Position targetPosition) {
+        int start = getPosition().getXOffset();
+        int end = targetPosition.getXOffset();
+        if (start > end)
+            return -1;
+        else
+            return 1;
+    }
+
+    private boolean validRookMove(Position targetPosition) {
+        return targetPosition.x == getPosition().x && targetPosition.y == getPosition().y;
+    }
+
+    private boolean getPositionOccupiedWithSameColor(Position targetPosition) {
+        ChessPiece targetPiece = getChessboard().getPieceAt(targetPosition);
+        if (targetPiece == null) {
+            if (targetPiece.getColor() == getColor())
+                return true;
+        }
+        return false;
     }
 
 }
