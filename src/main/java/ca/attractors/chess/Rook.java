6993ef3ff91fc5000f047520af62b869874d24bd
Mike Mogentale <mlmogentale@gmail.com>
2023-02-13 13:36:20 -0600

    remove duplicate position check

--- src/main/java/ca/attractors/chess/Rook.java
+++ src/main/java/ca/attractors/chess/Rook.java
@@ -15,57 +15,64 @@ public class Rook extends ChessPiece {
      */
     public boolean moveTo(Position targetPosition) {
         //if it is not the same x or y coordinate it is not a rooks valid move at all
-        if (validRookMove(targetPosition)) return false;
-
+        if (invalidRookMove(targetPosition)) return false;
         //Next - Check to make sure that if the target square is occupied it is not the same color
         if (getPositionOccupiedWithSameColor(targetPosition)) return false;
-
-        //Next - Get all the cells between the source and the target and ensure that they are empty.
-        // if this is a horizontal move we need to increment the y coordinate until it is the same as the target's y
-        // the increment might be positive or negative.
         if (checkXMoveInvalid(targetPosition)) return false;
-        //Next - Get all the cells between the source and the target and ensure that they are empty.
-        // if this is a vertical move we need to increment the x coordinate until it is the same as the target's x
-        // the increment might be positive or negative.
         if (checkYMoveInvalid(targetPosition)) return false;
         getChessboard().movePieceTo(this, targetPosition);
         return true;
     }
 
-    private boolean checkYMoveInvalid(Position targetPosition) {
+    private boolean checkXMoveInvalid(Position targetPosition) {
+        //Next - Get all the cells between the source and the target and ensure that they are empty.
+        // if this is a horizontal move we need to increment the y coordinate until it is the same as the target's y
+        // the increment might be positive or negative.
         if (targetPosition.y == getPosition().y) {
             int start = getPosition().getXOffset();
             int end = targetPosition.getXOffset();
             int increment = getIncrement(targetPosition);
-            List<Position> positions = getMovePositions(targetPosition, start, end, increment);
-            for (Position position: positions) {
-                if (getChessboard().getPieceAt(position) != null) {
-                    return true;
-                }
-            }
+            List<Position> positions = getMoveXPositions(targetPosition, start, end, increment);
+            if (moveBlocked(positions)) return true;
         }
         return false;
     }
 
-    private boolean checkXMoveInvalid(Position targetPosition) {
+    private boolean checkYMoveInvalid(Position targetPosition) {
+        //Next - Get all the cells between the source and the target and ensure that they are empty.
+        // if this is a vertical move we need to increment the x coordinate until it is the same as the target's x
+        // the increment might be positive or negative.
         if (targetPosition.x == getPosition().x) {
             int start = getPosition().getYOffset();
             int end = targetPosition.getYOffset();
             int increment = getIncrement(targetPosition);
-            List<Position> positions = getMovePositions(targetPosition, start, end, increment);
-            for (Position position: positions) {
-                if (getChessboard().getPieceAt(position) != null) {
-                    return true;
-                }
+            List<Position> positions = getMoveYPositions(targetPosition, start, end, increment);
+            if (moveBlocked(positions)) return true;
+        }
+        return false;
+    }
+
+    private boolean moveBlocked(List<Position> positions) {
+        for (Position position: positions) {
+            if (getChessboard().getPieceAt(position) != null) {
+                return true;
             }
         }
         return false;
     }
 
-    private static List<Position> getMovePositions(Position targetPosition, int start, int end, int increment) {
+    private static List<Position> getMoveYPositions(Position targetPosition, int start, int end, int increment) {
         List<Position> positions = new ArrayList<>();
         for (int y = start + increment; y != end; y = y + increment) {
             positions.add(Position.getPositionFor(targetPosition.getXOffset(), y));
+        }
+        return positions;
+    }
+
+    private static List<Position> getMoveXPositions(Position targetPosition, int start, int end, int increment) {
+        List<Position> positions = new ArrayList<>();
+        for (int x = start + increment; x != end; x = x + increment) {
+            positions.add(Position.getPositionFor(x, targetPosition.getYOffset()));
         }
         return positions;
     }
@@ -79,7 +86,7 @@ public class Rook extends ChessPiece {
             return 1;
     }
 
-    private boolean validRookMove(Position targetPosition) {
+    private boolean invalidRookMove(Position targetPosition) {
         return targetPosition.x != getPosition().x && targetPosition.y != getPosition().y;
     }
 
