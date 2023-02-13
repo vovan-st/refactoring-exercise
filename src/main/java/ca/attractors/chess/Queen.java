c5c6a44bb886ead65c6fd781d489a4ae8c722479
dpasion <pasiond@fcc.ca>
2023-02-13 14:49:31 -0600

    feat: add movement check

--- src/main/java/ca/attractors/chess/Queen.java
+++ src/main/java/ca/attractors/chess/Queen.java
@@ -15,9 +15,27 @@ public class Queen extends ChessPiece {
     }
 
     private boolean isTargetPositionValidMove(Position targetPosition){
-        boolean verticalMoveValid = true;
-        boolean horizontalMoveValid = true;
-        boolean diagonalMoveValid = true;
-        return verticalMoveValid && horizontalMoveValid && diagonalMoveValid;
+        boolean verticalMoveValid = isVerticalMovementValid(targetPosition);
+        boolean horizontalMoveValid = isHorizontalMovementValid(targetPosition);
+        boolean diagonalMoveValid = isDiagonalMoveValid(targetPosition);
+        return verticalMoveValid || horizontalMoveValid || diagonalMoveValid;
+    }
+
+    private boolean isVerticalMovementValid(Position targetPosition){
+        boolean isSameFile = targetPosition.x == getPosition().x;
+        boolean isSameRank = targetPosition.y == getPosition().y;
+        return  (isSameRank && !isSameFile) || (!isSameRank && isSameFile);
+    }
+
+    private boolean isHorizontalMovementValid(Position targetPosition){
+        boolean isSameFile = targetPosition.x == getPosition().x;
+        boolean isSameRank = targetPosition.y == getPosition().y;
+        return  (isSameRank && !isSameFile) || (!isSameRank && isSameFile);
+    }
+
+    private boolean isDiagonalMoveValid(Position targetPosition){
+        int movementX = Math.abs(targetPosition.x - this.getPosition().x);
+        int movementY = Math.abs(targetPosition.y - this.getPosition().y);
+        return movementY == movementX;
     }
 }
