d82c5223114a5d8864e39245a9d5aecedbfe3274
Mike Mogentale <mlmogentale@gmail.com>
2023-02-13 13:12:51 -0600

    ! logic

--- src/main/java/ca/attractors/chess/Rook.java
+++ src/main/java/ca/attractors/chess/Rook.java
@@ -15,12 +15,10 @@ public class Rook extends ChessPiece {
      */
     public boolean moveTo(Position targetPosition) {
         //if it is not the same x or y coordinate it is not a rooks valid move at all
-        if (!validRookMove(targetPosition)) {
-            return false;
-        }
+        if (validRookMove(targetPosition)) return false;
+
         //Next - Check to make sure that if the target square is occupied it is not the same color
-        if (getPositionOccupiedWithSameColor(targetPosition))
-            return false;
+        if (getPositionOccupiedWithSameColor(targetPosition)) return false;
 
         //Next - Get all the cells between the source and the target and ensure that they are empty.
         // if this is a horizontal move we need to increment the y coordinate until it is the same as the target's y
@@ -28,7 +26,7 @@ public class Rook extends ChessPiece {
         if (targetPosition.x == getPosition().x) {
             int start = getPosition().getYOffset();
             int end = targetPosition.getYOffset();
-            int increment = getIncrement(targetPosition)
+            int increment = getIncrement(targetPosition);
             List<Position> positions = new ArrayList<>();
             for (int y = start+increment; y != end; y = y + increment) {
                 positions.add(Position.getPositionFor(targetPosition.getXOffset(), y));
@@ -70,12 +68,12 @@ public class Rook extends ChessPiece {
     }
 
     private boolean validRookMove(Position targetPosition) {
-        return targetPosition.x == getPosition().x && targetPosition.y == getPosition().y;
+        return targetPosition.x != getPosition().x && targetPosition.y != getPosition().y;
     }
 
     private boolean getPositionOccupiedWithSameColor(Position targetPosition) {
         ChessPiece targetPiece = getChessboard().getPieceAt(targetPosition);
-        if (targetPiece == null) {
+        if (targetPiece != null) {
             if (targetPiece.getColor() == getColor())
                 return true;
         }
